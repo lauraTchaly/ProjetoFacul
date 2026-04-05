dfe48160b039a7bf8844ef9beb3d97bebecbaa92
lauraTchaly <lauramanes@outlook.com>
2026-04-05 09:28:18 -0300

    m4 erro

--- list/mylist/App.tsx
+++ list/mylist/App.tsx
@@ -1,12 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Login from './src/pages/login/Index';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Ol</Text>
       <StatusBar style="auto" />
-      <Login />
+      <Login/>
     </View>
   );
 }
---
+++ list/mylist/src/pages/login/Index.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+import {Text} from 'react-native';
+import {style} from "./style";
+
+
+export default function Login (){
+    return(
+        <view style={style.container}>
+            <view style={style.boxTop}>
+                <text>ola mundo</text>
+            </view>
+
+            <view style={style.boxMid}>
+            </view>
+
+            <view style={style.boxBottom}>   
+            </view>
+        </view>
+    )
+}
