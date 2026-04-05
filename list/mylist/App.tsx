209fb64c107c4bb719fcebb6ed2c313d6fd595dd
lauraTchaly <lauramanes@outlook.com>
2026-04-05 15:12:00 -0300

    m8

--- list/mylist/App.tsx
+++ list/mylist/App.tsx
@@ -1,21 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
+import './gesture-handler';
 import { StyleSheet, Text, View } from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import Routes from './src/routes/index.routes';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Ol</Text>
-      <StatusBar style="auto" />
-      <Login />
-    </View>
+    <NavigationContainer>
+      <Routes />
+    </NavigationContainer>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1
   },
 });
